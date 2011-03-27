ced1b07e573f10d1c4bfab8b9f023ad45b7d4262
Sawyer X <xsawyerx@cpan.org>
2011-03-27 12:07:14 +0300

    bumping version

--- lib/POE/Component/OpenSSH.pm
+++ lib/POE/Component/OpenSSH.pm
@@ -3,7 +3,7 @@ package POE::Component::OpenSSH;
 use strict;
 use warnings;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 use Moose;
 use Net::OpenSSH;
@@ -52,7 +52,7 @@ POE::Component::OpenSSH - Nonblocking SSH Component for POE using Net::OpenSSH
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =head1 SYNOPSIS
 
